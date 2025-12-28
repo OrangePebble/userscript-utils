939c1caa1893216ec03a9c3ecaec7ec776748843
p-laranjinha <60919073+p-laranjinha@users.noreply.github.com>
2025-12-28 15:26:26 +0000

    Updated the trash icon.
    Which is also how the script finds the real delete button, so its working again.

--- Youtube Mobile-like Playlist Remove Video Button/main.user.js
+++ Youtube Mobile-like Playlist Remove Video Button/main.user.js
@@ -196,7 +196,7 @@ function getYoutubeTrashSvg() {
 
 // This function is separate to find the menu's remove button in the observer
 function getSvgPathD() {
-  return "M11 17H9V8h2v9zm4-9h-2v9h2V8zm4-4v1h-1v16H6V5H5V4h4V3h6v1h4zm-2 1H7v15h10V5z";
+  return "M19 3h-4V2a1 1 0 00-1-1h-4a1 1 0 00-1 1v1H5a2 2 0 00-2 2h18a2 2 0 00-2-2ZM6 19V7H4v12a4 4 0 004 4h8a4 4 0 004-4V7h-2v12a2 2 0 01-2 2H8a2 2 0 01-2-2Zm4-11a1 1 0 00-1 1v8a1 1 0 102 0V9a1 1 0 00-1-1Zm4 0a1 1 0 00-1 1v8a1 1 0 002 0V9a1 1 0 00-1-1Z";
 }
 
 /**
